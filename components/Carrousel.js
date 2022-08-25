70f6a1df4bd74c03dd62dd485d83c33736f3d4c1
luciannomas <luciano.mastran@gmail.com>
2022-08-25 01:08:26 -0300

    second commit

---
+++ components/Carrousel.js
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://www.royalqueenseeds.es/img/cms/Critical%20B_1_1.jpg',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://www.royalqueenseeds.dk/img/cms/Choose-The-Right-Cannabis-Seeds-For-Your-Particular-Needs-Piramide.jpg',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://www.cannaconnection.com/modules/psblog/uploads/splash/1598019164.jpg',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://www.royalqueenseeds.es/img/cms/haze-cannabis.jpg',
+  },
+];
+
+function SwipeableTextMobileStepper() {
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 50,
+          pl: 2,
+          // bgcolor: 'background.default',
+          backgroundColor: 'transparent',
+          marginTop: -2
+        }}
+      >
+        {/* <Typography>{images[activeStep].label}</Typography> */}
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+        style={{ borderRadius: 36 , width: '60pc'}}
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                style={{borderRadius: '18px', display: 'initial'}}
+                sx={{
+                  height: 455,
+                  display: 'block',
+                  // maxWidth: '10%',
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.imgPath}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === maxSteps - 1}
+            style={{ marginTop: -300 }}
+          >
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button style={{ marginTop: -300 }} size="medium" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === 'rtl' ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+          </Button>
+        }
+      />
+    </Box>
+  );
+}
+
+export default SwipeableTextMobileStepper;
