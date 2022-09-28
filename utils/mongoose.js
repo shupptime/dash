443b11ec65f6e6afaa04d26e376c14df58d62fff
luciannomas <luciano.mastran@gmail.com>
2022-09-28 00:50:00 -0300

    change bd mongo-cluster

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -5,7 +5,7 @@ const conn = {
 };
 
 export async function dbConnect() {
-  const db = await connect('mongodb://localhost/eat-ser');
+  const db = await connect('mongodb+srv://vercel-admin-user:admin123@cluster0.tofqzz9.mongodb.net/eat-ser');
   conn.isConnected = db.connections[0].readyState;
 }
 
