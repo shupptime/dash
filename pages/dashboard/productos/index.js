cefdba5fa3c5fc36f126dea0e16b558263a3ee3d
luciano <luciano.mastran@gmail.com>
2023-02-14 04:23:11 -0300

    add media-query in the component divider

--- pages/dashboard/productos/index.js
+++ pages/dashboard/productos/index.js
@@ -9,7 +9,10 @@ import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
 
+//media-query
+import useMediaQuery from "@mui/material/useMediaQuery";
 //component
+
 import Navbar from '../../../components/Navbar';
 import ProductsDash from '../../../components/ProductsDash';
 
@@ -18,82 +21,91 @@ import ProductsDash from '../../../components/ProductsDash';
 import { useRouter } from "next/router";
 
 const Item = styled(Paper)(({ theme }) => ({
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const style = {
-    width: '100%',
-    maxWidth: 360,
-    bgcolor: 'background.paper',
-    marginTop: '50px',
-    };
+  width: '100%',
+  // maxWidth: 360,
+  bgcolor: 'background.paper',
+  marginTop: '50px',
+};
 
 
-export default function Products({categoria}) {
-    const router = useRouter();
+export default function Products({ categoria }) {
+  const router = useRouter();
+  const matches = useMediaQuery('(max-width:600px)');
+
   return (
     <>
-        <Navbar />
-        
-        <Box sx={{ flexGrow: 1, padding: "3pc", marginTop:"45px"}}>
-            
-            <Typography variant="h4" sx={{ mb: 5, textAlign: 'center' }}>
-                Elige una Categoria
-            </Typography>
-            
-            {/* <ProductsDash key={3} /> */}
-            <List sx={style}  component="nav" aria-label="mailbox folders">
-                {
-                    categoria.map((cat) => (
-                    <div key = {cat._id} > 
-                        <ListItem button> 
-                            <ListItemText 
-                              style={{ textAlign: 'center'}} 
-                              primary={cat.title}  
-                              onClick={ ()=> { router.push(`/dashboard/productos/${cat._id}/dash`) }} />
-                        </ListItem>
-                        <Divider />
-                    </div>
-                    ))
-                }
+      <Navbar />
+
+      <Box sx={{ flexGrow: 1, padding: 5, marginTop: "45px" }}>
+
+        <Typography variant="h4" sx={{ mb: 5, textAlign: 'center' }}>
+          Elige una Categoria
+        </Typography>
+
+        {/* <ProductsDash key={3} /> */}
+        <List component="nav" aria-label="mailbox folders">
+          {
+            categoria.map((cat) => (
+              <div key={cat._id} >
+                <ListItem button>
+                  <ListItemText
+                    style={{ textAlign: 'center' }}
+                    primary={cat.title}
+                    onClick={() => { router.push(`/dashboard/productos/${cat._id}/dash`) }} />
+                </ListItem>
+                <Divider variant="middle" style={!matches ? { marginLeft: 650, marginRight: 650 } : { width: '90%' }} />
+              </div>
+            ))
+          }
         </List>
-            
-            <Button 
-                sx = {{ background:'radial-gradient(orange, transparent)', marginTop: '50px', marginLeft: '12px', width: '90%', height: '55px', fontSize: "15px"}}
-                variant="contained" size="large"
-                onClick={ ()=> { router.push(`/dashboard`) }}
-            >
-                Volver
-            </Button>   
-        
-        </Box>
+        <div style={{ textAlign: 'center' }}>
+          <Button
+            sx={!matches ? {
+              background: 'radial-gradient(orange, transparent)',
+              marginTop: '50px',
+              width: '30%',
+              height: '55px',
+              fontSize: "15px"
+            } : { background: 'radial-gradient(orange, transparent)', marginTop: '50px', width: '100%', height: '55px', fontSize: "15px" }}
+            variant="contained" size="large"
+            onClick={() => { router.push(`/dashboard`) }}
+          >
+            Volver
+          </Button>
+        </div>
+
+
+      </Box>
     </>
-);
+  );
 }
 
 export async function getServerSideProps() {
-    const res = await fetch("https://eat-ser.vercel.app/api/categorias");
-    
-    if (res.status === 200) {
-      const categoria = await res.json();
-      return {
-        props: {
-          categoria,
-        },
-      };
-    }
-  
+  const res = await fetch("https://eat-ser.vercel.app/api/categorias");
+
+  if (res.status === 200) {
+    const categoria = await res.json();
     return {
       props: {
-        error: {
-          statusCode: res.status,
-          statusText: "not table",
-        },
+        categoria,
       },
     };
   }
 
-  
+  return {
+    props: {
+      error: {
+        statusCode: res.status,
+        statusText: "not table",
+      },
+    },
+  };
+}
+
